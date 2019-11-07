bc798d6024646f9cf15bb69dc6c30840a70fe086
Reuben Dunn <reuben.dunn@hypr.co.nz>
2019-11-07 15:13:45 +1300

    Updated tgw examples

---
+++ tgw/example/ram-share/main.tf
@@ -0,0 +1,35 @@
+module "example" {
+  source = "../../"
+
+  providers = {
+    aws = "aws"
+  }
+
+  name        = "example-tgw"
+  description = "An Example TGW via Terraform"
+  tags        = "${map("Environment", format("%s", "stage"))}"
+
+  connecting_aws_account_numbers = ["1234567890"]
+
+  auto_accept_shared_attachments  = "enable"
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+}
+
+variable "aws_region" {
+  default = "ap-southeast-2"
+}
+
+variable "aws_account_number" {
+  default = ""
+}
+
+output "tgw-arn" {
+  value = "${module.example.arn}"
+}
+
+output "tgw-id" {
+  value = "${module.example.id}"
+}
+
+
