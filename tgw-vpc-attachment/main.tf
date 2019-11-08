4e47b9f0e5d0ab611cda647d3c4b43709d235dff
Reuben Dunn <reuben.dunn@hypr.co.nz>
2019-11-08 13:04:54 +1300

    First iterration of Transit Gateway Attachment Module

---
+++ tgw-vpc-attachment/main.tf
@@ -0,0 +1,13 @@
+resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
+  subnet_ids         = ["${var.vpc_subnet_ids}"]
+  transit_gateway_id = "${var.transit_gateway_id}"
+  vpc_id             = "${var.vpc_id}"
+
+  transit_gateway_default_route_table_association = "${var.create_default_route_table_association}"
+  transit_gateway_default_route_table_propagation = "${var.create_default_route_table_propagation}"
+
+  dns_support  = "${var.dns_support}"
+  ipv6_support = "${var.ipv6_support}"
+
+  tags = "${var.tags}"
+}
---
+++ tgw-vpc-attachment/vars.tf
@@ -0,0 +1,39 @@
+variable "vpc_subnet_ids" {
+  description = "A list of subnets for routing via the Transit Gateway Attachment. Can only be 1 subnet per AZ"
+  type        = "list"
+}
+
+variable "transit_gateway_id" {
+  description = "The id of the Transit Gateway the Attahcment should bind to"
+}
+
+variable "vpc_id" {
+  description = "The VPC id for the Attachment"
+}
+
+variable "create_default_route_table_association" {
+  description = "Whether the VPC Attachment should be associated with the EC2 Transit Gateway association default route table. Default value: `true`."
+  default     = true
+}
+variable "create_default_route_table_propagation" {
+  description = "Whether the VPC Attachment should propagate routes with the EC2 Transit Gateway propagation default route table. Default value: true."
+  default     = true
+}
+
+variable "dns_support" {
+  description = "Whether DNS support is enabled. Valid values: `disable`, `enable`. Default value: `enable`."
+  default     = "enable"
+}
+
+variable "ipv6_support" {
+  description = "Whether IPv6 support is enabled. Valid values: `disable`, `enable`. Default value: `disable`."
+  default     = "disable"
+}
+
+variable "tags" {
+  description = "a map of tags to add to Attachment Resource"
+  type        = "map"
+  default     = {}
+}
+
+
